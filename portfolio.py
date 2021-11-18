b96923aafb614e6b066f782393089d81cf31429f
77zhang77 <93165961+77zhang77@users.noreply.github.com>
2021-11-18 14:40:24 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -11,11 +11,11 @@ and has a second method called cost which returns a float, the total cost paid f
 Consider that to implement the cost method, you'll need to be storing the purchases made each time the buy method is called somewhere. You may do so by any means convenient to you.
 
 """
-
 class Portfolio:
     def __init__(self):
         self.stocks=[]
-    def buy(self,name,share,price):
+    def buy(self,name,shares,price):
         self.stocks.append((name,shares,price))
+
     def cost(self):
         return sum(shares*price for _,shares,price in self.stocks)
